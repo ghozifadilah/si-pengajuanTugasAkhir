4b953c6c13a5d476848355d841c9d0a15136b85e
rizkahaqiqi <rizkanurulkhakiki@gmail.com>
2018-05-22 08:51:16 +0700

    formmahasiswaview

---
+++ application/views/formmahasiswaview.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<div class="container">
+  <h2>FORM PENGAJUAN JUDUL TUGAS AKHIR</h2>
+  <form>
+    <div class="form-group">
+      <label for="usr">NAMA :</label>
+      <input type="text" class="form-control" id="usr">
+    </div>
+    <div class="form-group">
+      <label for="nim">NIM :</label>
+      <input type="texy" class="form-control" id="nim">
+    </div>
+  </form>  
+  </form>GOLONGAN :</div>
+<div class="radio">
+  <label><input type="radio" name="optradio">A</label>
+</div>
+<div class="radio">
+  <label><input type="radio" name="optradio">B</label>
+</div>
+<div class="radio disabled">
+  <label><input type="radio" name="optradio">C</label>
+</div>
+<div class="radio disabled">
+  <label><input type="radio" name="optradio">D</label>
+</div>
+ <div class="form-group">
+      <label for="nim">JUDUL :</label>
+      <input type="texy" class="form-control" id="judul">
+</div>
+<div class="form-group">
+  <label for="sel1">USULAN DOSEN PEMBIMBING:</label>
+  <select class="form-control" id="sel1">
+    <option>Hermawan</option>
+    <option>Taufik</option>
+    <option selected="selected">Nanik</option>
+    <option>Betty</option>
+  </select>
+</div>
+   <div class="form-group">
+      <p>
+        <label for="nim">RINGKASAN :</label>
+        <input type="texy" class="form-control" id="ringkasan">
+      </p>
+   </div>
+    <div class="container">
+  <button type="button" class="btn">SETORKAN</button>
+</div>
+</body>
+</html>
