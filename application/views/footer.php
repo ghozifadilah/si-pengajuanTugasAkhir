499f5643c2f9e6e0f46ada884c3c16fafb90214e
rizkahaqiqi <rizkanurulkhakiki@gmail.com>
2018-05-28 10:58:52 +0700

    Headerfooter

---
+++ application/views/footer.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<p>   <!-- Footer -->
+    <footer class="py-5 bg-dark">
+<div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Manajemen Informatika 2018</p>
+      </div>
+      <!-- /.container -->
+   
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="<?php echo base_url('vendor/jquery/jquery.min.js')?>"></script>
+    <script src="<?php echo base_url('vendor/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="<?php echo base_url('vendor/jquery-easing/jquery.easing.min.js')?>"></script>
+
+    <!-- Custom JavaScript for this theme -->
+    <script src="<?php echo base_url('js/scrolling-nav.js')?>"></script>
+
+</body>
+
+</html></p>
+</body>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title><?php echo $title ?></title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url('assets/vendor/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="<?php echo base_url('assets/css/scrolling-nav.css')?>" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+      <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">JURUSAN TEKONOLOGI INFORMASI POLTIKENIK NEGERI JEMBER</a></div>
+    </nav>
