313ffdc2245605f989561d76cd5ef4f6893a60b3
Ghozi Fadhillah Himma <ghozifadilah@users.noreply.github.com>
2018-06-28 07:25:39 +0700

    Desain proposal

---
+++ application/views/mahasiswa/v_proposal.php
@@ -0,0 +1,30 @@
+<header class="bg-secondary text-white">
+      <div class="container text-center"></div>
+    </header>
+
+    <section id="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+         <div class="box-form">
+<h3>Form Pendaftaran Ujian Proposal</h3>
+
+	
+ 
+  <?php echo form_open_multipart('mahasiswa/homeproposal/aksi_upload');?>
+ 
+	
+             Judul:<br>
+             <textarea name="judul" rows="3" cols="40"></textarea><br><br>
+           </p>
+ 
+  <input type="file" name="berkas" />
+ 
+  <br /><br />
+ 
+  <input type="submit" value="Upload" />
+ 
+</form>
+ 
+</body>
+</html>
