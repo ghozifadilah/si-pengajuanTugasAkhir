12a0f335e12bf299509e454c61f2a008e6f28fcf
Ghozi Fadhillah Himma <ghozifadilah@users.noreply.github.com>
2018-06-28 07:59:44 +0700

    view_Statistik usulan

--- application/views/Statistik/Chart_viewDosen.php
+++ application/views/Statistik/Chart_viewDosen.php
@@ -72,37 +72,35 @@
   </script>
 
    <!--Script Grafik Pengajuan yg belum atau sudah -->
-   <script type="text/javascript">
-    google.charts.load("current", {packages:["corechart"]});
-    google.charts.setOnLoadCallback(drawChart);
-    function drawChart() {
-      var data = google.visualization.arrayToDataTable([
-        ["Element", "Density", { role: "style" } ],
-        ["Copper", 8.94, "#b87333"],
-        ["Silver", 10.49, "silver"],
-        ["Gold", 19.30, "gold"],
-        ["Platinum", 21.45, "color: #e5e4e2"]
-      ]);
+   <script type="text/javascript"> 
+     
+    // Load the Visualization API and the piechart package. 
+    google.charts.load('current', {'packages':['corechart']}); 
+       
+    // Set a callback to run when the Google Visualization API is loaded. 
+    google.charts.setOnLoadCallback(drawChart); 
+       
+    function drawChart() { 
+      var jsonData = $.ajax({ 
+          url: "<?php echo base_url() . 'index.php/Statistik/GrafikMahasiswa/getdata' ?>", 
+          dataType: "json", 
+          async: false 
+          }).responseText; 
+      
+	  var options = {
+          title: 'Data Sebaran Dosen'
+        };
 
-      var view = new google.visualization.DataView(data);
-      view.setColumns([0, 1,
-                       { calc: "stringify",
-                         sourceColumn: 1,
-                         type: "string",
-                         role: "annotation" },
-                       2]);
-
-      var options = {
-        title: "Density of Precious Metals, in g/cm^3",
-        width: 600,
-        height: 400,
-        bar: {groupWidth: "95%"},
-        legend: { position: "none" },
-      };
-      var chart = new google.visualization.BarChart(document.getElementById("barchart_values"));
-      chart.draw(view, options);
-  }
-  </script>
+      // Create our data table out of JSON data loaded from server. 
+      var data = new google.visualization.DataTable(jsonData); 
+ 
+      // Instantiate and draw our chart, passing in some options. 
+      var chart = new google.visualization.PieChart(document.getElementById('chartmahasiswa_div')); 
+      chart.draw(data, {width: 900, height: 500}); 
+	  chart.draw(data, google.charts.Pie.convertOptions(options));
+    } 
+ 
+    </script> 
 	
 	
 	
@@ -119,7 +117,7 @@ h1 {
     <h1>Quantity of fruits we have in our store - Displayed by Google Chart and Codeigniter with MySQL</h1> 
     <div id="chart_div"></div> 
 	<div id="line_top_x"></div> 
-	<div id="barchart_values" style="width: 900px; height: 300px;"></div>
+	<div id="chartmahasiswa_div"></div> 
 	<br><br><br>
   </body> 
 </html>
