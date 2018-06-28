cb817b3a9e4a5095f1043026b5a564695f410db9
Ghozi Fadhillah Himma <ghozifadilah@users.noreply.github.com>
2018-06-28 09:10:08 +0700

    Edit-judul

--- application/views/dosen/Edit-Judul.php
+++ application/views/dosen/Edit-Judul.php
@@ -1,60 +1,22 @@
 <html>
   <head>
     <title>Ajukan Judul|| SITA </title>
-   <!-- <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/util.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/main.css">-->
   </head>
   <body>
       <center>
         <h1>Halaman Pengajuan Judul || SITA </h1><br />
 		<h3>Edit Data</h3><br /><br />
-
-		<a href="<?php echo site_url('dosen/dosen'); ?>">Home</a>
+		<a href="<?php echo site_url('mahasiswa/mahasiswa'); ?>">Home</a>
         <br>
-		<a href="<?php echo site_url('dosen/penelitian'); ?>">LihatJudul</a>
+		<a href="<?php echo site_url('mahasiswa/mahasiswa/LihatJudul'); ?>">LihatJudul</a>
+        <br>
+		<a href="<?php echo site_url('authentication/auth/logout'); ?>">Keluar</a>
       </center>
 	  <br>
 	  <br>
 	
 			
 	</center>
-	<center><!---
-<div class="wrap-login100 p-l-50 p-r-50 p-t-72 p-b-50">
-
-					<div class="wrap-input100 validate-input" data-validate>
-						<span class="label-input100">Username</span><br>
-						<textarea type="hidden" name="id" class="input100"><?php echo $u->id_user ?></textarea>
-						
-						<span class="focus-input100"></span>
-					</div>
-
-					<div class="wrap-input100 validate-input" data-validate>
-						<span class="label-input100">Judul Tugas Akhir</span><br>
-						<textarea type="text" name="Judul" class="input100"><?php echo $u->Judul_Penelitian ?></textarea>
-						
-						<span class="focus-input100"></span>
-					</div>
-
-					<div class="wrap-input100 validate-input" data-validate>
-						<span class="label-input100">Kuota Tugas Akhir</span><br>
-						<textarea type="text" name="kuota" class="input100"><?php echo $u->kuota ?></textarea>
-						
-						<span class="focus-input100"></span>
-					</div>
-
-					<div class="container-login100-form-btn">
-						<div class="wrap-login100-form-btn">
-							<div class="login100-form-bgbtn"></div>
-							<center><button class="login100-form-btn" type="submit" name="btnTambah" value="Simpan">
-								Simpan
-							</button></center>
-						</div>
-
-	<script src="<?php echo base_url(''); ?>assets/vendor/jquery/jquery-3.2.1.min.js"></script>
-	<script src="<?php echo base_url(''); ?>assets/js/main.js"></script>
-
-</center>-->
-<!--===============================================================================================-->
 	<?php foreach($penelitian as $u){ ?>
 	<form action="<?php echo base_url(). 'dosen/penelitian/update'; ?>" method="post">
 		<table style="margin:20px auto;">
@@ -75,6 +37,9 @@
 			</tr>
 		</table>
 	</form>	
-	<?php } ?>  	
+	<?php } ?>
+
+  	
+  
   </body>
 </html>
